d6a2ce7515532b1825c84eebbdd16bce7e749084
okoala <dapixp@gmail.com>
2017-12-02 14:05:55 +0800

    fix: filepath link error

--- agent.js
+++ agent.js
@@ -1,19 +1,49 @@
 const { resolve } = require('path');
-const { readFile } = require('fs');
+const { readFile, existsSync } = require('fs');
 const MonitorServer = require('webpack-monitor/utils/server');
+
+function waitOnFile(file, setup = 15000, interval = 4000, timeout = 60000) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (existsSync(file)) {
+        return resolve(true);
+      }
+      const now = new Date();
+      let timer = setInterval(() => {
+        if (existsSync(file)) {
+          resolve(true);
+          clearInterval(timer);
+          timer = null;
+          return;
+        }
+        const t = new Date();
+        if (t - now > timeout) {
+          reject(new Error('avet monitor wait file time out!'));
+        }
+      }, interval);
+    }, setup);
+  });
+}
 
 module.exports = ({ config }) => {
   const { launch, port, target } = config.monitor;
   if (launch) {
-    setTimeout(() => {
-      const filepath = resolve(__dirname, 'node_modules', target);
-      readFile(filepath, 'utf8', (err, data) => {
-        if (err) {
-          throw err;
-        }
-
-        MonitorServer(JSON.parse(data), port);
+    const filepath = resolve(
+      require.resolve('webpack-monitor'),
+      '../..',
+      target
+    );
+    waitOnFile(filepath)
+      .then(() => {
+        readFile(filepath, 'utf8', (err, data) => {
+          if (err) {
+            throw err;
+          }
+          MonitorServer(JSON.parse(data), port);
+        });
+      })
+      .catch(err => {
+        throw err;
       });
-    }, 6000);
   }
 };
